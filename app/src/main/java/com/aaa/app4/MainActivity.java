c1d50f7acd2d7523b92396aa1a933453ea0eac33
aliciawill <57860276+aliciawill@users.noreply.github.com>
2021-10-17 22:06:38 +0900

    Update MainActivity.java
    내가 바꿔봤어...이게 나을거 같아.

--- app/src/main/java/com/aaa/app4/MainActivity.java
+++ app/src/main/java/com/aaa/app4/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d("git>> ", "내용 또또....변경됨2..");
+        //여기를 좀 바꾸면 어떨까..그러면 정말 좋겠어..!!!
     }
 }
